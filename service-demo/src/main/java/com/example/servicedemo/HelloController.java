79b640f06933a74d62b465740627219de7132437
Aida <643074224@qq.com>
2018-06-20 23:32:08 +0800

    服务注册中心修改

--- service-demo/src/main/java/com/example/servicedemo/HelloController.java
+++ service-demo/src/main/java/com/example/servicedemo/HelloController.java
@@ -4,13 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * @author 作者 E-mail:
- * @version 创建时间：${date} ${time}
- * 类说明
- */
+
 @Controller
 public class HelloController {
+
     @RequestMapping("/")
     @ResponseBody
     public  String hello()
